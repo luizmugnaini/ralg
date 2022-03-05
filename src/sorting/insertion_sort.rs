419e2d336bdd2c36dfa91a0a01647bf277b9e57f
Luiz Mugnaini <luizmugnaini@gmail.com>
2022-03-04 23:04:58 -0300

    insertion sort
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

---
+++ src/sorting/insertion_sort.rs
@@ -0,0 +1,29 @@
+use crate::sorting::Sorter;
+use std::cmp::PartialOrd;
+
+pub struct InsertionSort;
+
+impl Sorter for InsertionSort {
+    fn sort<T: PartialOrd + Copy>(xs: &mut [T]) {
+        for not_sorted in 1..xs.len() {
+            let mut i = not_sorted;
+            while i > 0 && xs[i - 1] > xs[i] {
+                xs.swap(i - 1, i);
+                i -= 1;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::sorting;
+
+    #[test]
+    fn insertion() {
+        let mut xs = vec![123, 91847, 1, 0, -1, -450, 800, 555];
+        InsertionSort::sort(&mut xs);
+        assert!(sorting::is_sorted(&xs));
+    }
+}
---
+++ src/sorting/mod.rs
@@ -0,0 +1,32 @@
+mod insertion_sort;
+
+use std::cmp::PartialOrd;
+
+trait Sorter {
+    fn sort<T: PartialOrd + Copy>(xs: &mut [T]);
+}
+
+pub fn is_sorted(xs: &[impl PartialOrd]) -> bool {
+    let mut last = &xs[0];
+    for next in xs {
+        if last > next {
+            return false;
+        }
+        last = next;
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sorted() {
+        let v1 = vec![1, 2, 3, 4, 5, 5];
+        assert!(is_sorted(&v1));
+
+        let v2 = vec![0, 3, 9, 8, 10];
+        assert!(!is_sorted(&v2));
+    }
+}
