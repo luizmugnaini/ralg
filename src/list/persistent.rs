2cfb2f9d34f97271b5648eeae77e9c4a8694d78b
Luiz Mugnaini <luizmugnaini@gmail.com>
2022-03-06 00:52:34 -0300

    Linked lists: single linked and persistent
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

---
+++ src/list/persistent.rs
@@ -0,0 +1,110 @@
+use std::rc::Rc;
+
+pub struct Persistant<T> {
+    head: List<T>,
+}
+
+type List<T> = Option<Rc<Node<T>>>;
+
+impl<T> Persistant<T> {
+    pub fn new() -> Self {
+        Self { head: None }
+    }
+
+    pub fn prepend(&self, key: T) -> Self {
+        Self {
+            head: Some(Rc::new(Node::new(key, self.head.clone()))),
+        }
+    }
+
+    pub fn tail(&self) -> Self {
+        Self {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.key)
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.key
+        })
+    }
+}
+
+impl<T> Drop for Persistant<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            match Rc::try_unwrap(node) {
+                Ok(mut node) => head = node.next.take(),
+                _ => break,
+            }
+        }
+    }
+}
+
+struct Node<T> {
+    key: T,
+    next: List<T>,
+}
+
+impl<T> Node<T> {
+    fn new(key: T, next: List<T>) -> Self {
+        Self { key, next }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let list = Persistant::new();
+        // Check head and tail in empty case
+        assert_eq!(list.head(), None);
+        assert_eq!(list.tail().head(), None);
+
+        let list = list.prepend(0).prepend(1).prepend(2);
+        assert_eq!(list.head(), Some(&2));
+
+        // Check tail
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&0));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = Persistant::new().prepend(0).prepend(1).prepend(2);
+
+        let mut iter = list.iter();
+        for x in (0..3).rev() {
+            assert_eq!(iter.next(), Some(&x));
+        }
+        assert_eq!(iter.next(), None);
+    }
+}
---
+++ src/list/single.rs
@@ -0,0 +1,186 @@
+pub struct SingleLinked<T> {
+    head: List<T>,
+}
+
+type List<T> = Option<Box<Node<T>>>;
+
+impl<T> SingleLinked<T> {
+    pub fn new() -> Self {
+        Self { head: None }
+    }
+
+    pub fn push(&mut self, key: T) {
+        let node = Box::new(Node::new(key, self.head.take()));
+
+        self.head = Some(node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.key
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.key)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.key)
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
+}
+
+pub struct IntoIter<T>(SingleLinked<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.key
+        })
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.key
+        })
+    }
+}
+
+impl<T> Drop for SingleLinked<T> {
+    fn drop(&mut self) {
+        let mut list = self.head.take();
+        while let Some(mut node) = list {
+            list = node.next.take();
+        }
+    }
+}
+
+struct Node<T> {
+    key: T,
+    next: List<T>,
+}
+
+impl<T> Node<T> {
+    fn new(key: T, next: List<T>) -> Self {
+        Node { key, next }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn push_n_pop() {
+        let mut list = SingleLinked::new();
+
+        // Verify the correctness of pop for the empty case
+        assert_eq!(list.pop(), None);
+
+        for x in 0..4 {
+            list.push(x);
+        }
+
+        for x in (0..4).rev() {
+            assert_eq!(list.pop(), Some(x));
+        }
+
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = SingleLinked::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+
+        list.push(1);
+        assert_eq!(list.peek(), Some(&1));
+
+        list.peek_mut().map(|key| {
+            *key += 2;
+        });
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        assert_eq!(list.peek(), Some(&3));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = SingleLinked::new();
+        for x in 0..3 {
+            list.push(x);
+        }
+        let mut iter = list.into_iter();
+        for x in (0..3).rev() {
+            assert_eq!(iter.next(), Some(x));
+        }
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = SingleLinked::new();
+        for x in 0..3 {
+            list.push(x);
+        }
+        let mut iter = list.iter();
+        for x in (0..3).rev() {
+            assert_eq!(iter.next(), Some(&x));
+        }
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = SingleLinked::new();
+        for x in 0..3 {
+            list.push(x);
+        }
+        let mut iter = list.iter_mut();
+        for mut x in (0..3).rev() {
+            assert_eq!(iter.next(), Some(&mut x));
+        }
+        assert_eq!(iter.next(), None);
+    }
+}
